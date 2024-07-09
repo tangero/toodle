709fe3d0f3215034de809f93bb2d9ccc72e1db23
Patrick Zandl <tangero@users.noreply.github.com>
2024-07-09 12:03:32 +0200

    signals pryc

--- moje_aplikace/apps.py
+++ moje_aplikace/apps.py
@@ -1,10 +1,10 @@
 from django.apps import AppConfig
 
-
 class MojeAplikaceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'moje_aplikace'
     verbose_name = 'Toodle'
 
-    def ready(self):
-        import moje_aplikace.signals  # noqa
+    # Pokud nemáte signals.py, můžete tuto metodu zcela odstranit nebo zakomentovat
+    # def ready(self):
+    #     import moje_aplikace.signals  # noqa
