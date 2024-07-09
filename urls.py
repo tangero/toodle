f394b92fb5bf4ef5048bfe538536dce0ac9d1df3
Patrick Zandl <patrick.zandl@marigold.cz>
2024-07-09 10:20:31 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,7 +1,13 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('moje_aplikace.urls')),
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('add_text/', views.add_text, name='add_text'),
+    path('text_list/', views.text_list, name='text_list'),
+    path('hashtag_cloud/', views.hashtag_cloud, name='hashtag_cloud'),
 ]
