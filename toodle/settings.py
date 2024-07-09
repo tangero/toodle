fb2bfe86728e37f1e841c1870fac3e2ae53e9959
Patrick Zandl <tangero@users.noreply.github.com>
2024-07-09 12:07:59 +0200

    host

--- toodle/settings.py
+++ toodle/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'django-insecure-NAHRAĎTE-TOTO-SKUTEČNÝM-TAJNÝM-KLÍČEM'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['toodle-47kv.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
